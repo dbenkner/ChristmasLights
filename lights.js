038f31c41ea0fc80fb7109772b9fdc555b0cb00d
David Benkner <d.benkner@gmail.com>
2023-12-01 16:04:24 -0500

    Many changes

--- lights.js
+++ lights.js
@@ -1,26 +1,33 @@
-
 const getWidth = function ()  {
     let width = $(".banner").width();
     console.log(width);
-    numLights = Math.floor(width / 30);
+    let numLights = Math.floor(width / 32);
     console.log(numLights);
     let lightId = 1;
     $(".banner").empty();
     for(let i = 0; i < numLights; i++) {
-        $(".banner").append(`<div class="light" id=light${lightId}></div>`);
+        $(".banner").append(`<div class="light light${lightId}"</div>`);
         lightId++;
-        if(lightId == 6) lightId = 1;
+        if(lightId === 7) lightId = 1;
     }
-}
-let op = 100;
-const flash = () => {
-    if(op === 100) {
-        op = 50;
-    } else {
-        op = 100;
-    }
-    $(".light").css("opacity", `${op}%` );
 }
 
+let op = 100;
+let lightId = 1;
+let lastLight = lightId;
+const flash = () => {
+    $(`.light${lastLight}`).css("opacity", `100%` );
+    $(`.light${lightId}`).css("opacity", `25%` );
+    lastLight = lightId;
+    lightId ++;
+    if(lightId === 7) {
+        lightId = 1;
+    }
+}
+var sound = new Howl({
+    src: ['song.mp3']
+});
+$(document).ready(getWidth());
+sound.play();
 setInterval(flash, 500);
 window.onresize = getWidth;
